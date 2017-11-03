19380ed7e98a6dd4cb46b98755bec352ce63cee2
Aurash Norouzi <aurashn@users.noreply.github.com>
2017-11-03 16:38:27 -0500

    Added PID control function

--- ADC&PWM/main.c
+++ ADC&PWM/main.c
@@ -55,13 +55,20 @@ void ledToggle();
 void uart1int();
 void motorMove(unsigned long leftPower, unsigned long rightPower, int dir);
 void motorStop();
-void distFront();
-void distRight();
+float distFront();
+float distRight();
 void adc_init();
 
 volatile int16_t i16ToggleCount=0;
+float error_prior = 0;
+float integral = 0;
+float kp = .2;
+float ki = .2;
+float kd = .3;
 uint32_t ui32ADC1Value[2] = {};
 uint32_t ui32ADC0Value[2] = {};
+uint32_t result=0;
+uint32_t result1=0;
 int count = 0;
 char comms[200];
 
@@ -105,14 +112,17 @@ void uart1int(void){
                 if(comms[count-2]=='s' && comms[count-1]=='s') {
                     motorStop();
                 }
+                if(comms[count-2]=='p' && comms[count-1]=='d') {
+                    TimerEnable(TIMER2_BASE, TIMER_A);
+                }
                 if(comms[count-2]=='o' && comms[count-1]=='f')
                 GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_1|GPIO_PIN_2|GPIO_PIN_3, 0);
                 UARTCharPutNonBlocking(UART1_BASE, '\n');
                 UARTCharPutNonBlocking(UART1_BASE, '\r');
-                writeStringToUart1("You just said");
-                UARTCharPutNonBlocking(UART1_BASE, ' ');
-                writeStringToUart1(comms);
-                writeStringToUart1("\n\rLet's Talk: \n\r\t");
+                //writeStringToUart1("You just said");
+                //UARTCharPutNonBlocking(UART1_BASE, ' ');
+                //writeStringToUart1(comms);
+                //writeStringToUart1("\n\rLet's Talk: \n\r\t");
                 count = 0;
                 memset(comms,'\0',sizeof(comms));
                 break;
@@ -123,6 +133,19 @@ void uart1int(void){
       }
 }
 
+
+void PID_start()
+    {
+        float error=0;
+        error= 4 - distRight();
+        integral = integral + (error*.50);
+        float derivative = (error-error_prior)/.50;
+        float output = kp*error;//+kd*derivative;//+ki*integral+kd*derivative;
+        error_prior=error;
+        if(output>.05){motorMove(200,100,0);}
+        if(output<-.05){motorMove(100,200,0);}
+        else{motorMove(200,200,0);}
+    }
 void ledToggle(void)
 {
     // LED values - 2=RED, 4=BLUE, 8=GREEN
@@ -159,55 +182,66 @@ void motorStop(){
     PWMOutputState (PWM1_BASE, PWM_OUT_3_BIT | PWM_OUT_2_BIT, false);
 }
 
-void distRight(){
+float distRight(){
         //Clear interrupt flag
-        ADCIntClear(ADC0_BASE, 2);
+        ADCIntClear(ADC0_BASE, 3);
         //Trigger ADC
-        ADCProcessorTrigger(ADC0_BASE, 2);
+        ADCProcessorTrigger(ADC0_BASE, 3);
         //wait for conversion
-        while(!ADCIntStatus(ADC0_BASE, 2, false)){}
+        while(!ADCIntStatus(ADC0_BASE, 3, false)){}
         //retrieve data
-        ADCSequenceDataGet(ADC0_BASE, 2, ui32ADC0Value);
+        ADCSequenceDataGet(ADC0_BASE, 3, &result);
 
-        *ui32ADC0Value= (2080*7)/(*ui32ADC0Value);
-        /* Convert digital back to analog */
-
-        char  str[50];
-        sprintf(str,"Right: %u cm\n",ui32ADC0Value);
-        writeStringToUart1(str);
+        result= (2080*7)/(result);
+        return (float)result;
+        //char  str[50] = "hello";
+        //sprintf(str,"Right: %u cm",result);
+        //writeStringToUart1(str);
 
 }
 
-void distFront(){
+float distFront(){
 
-    //Clear interrupt flag
-          ADCIntClear(ADC1_BASE, 2);
+          //Clear interrupt flag
+          ADCIntClear(ADC1_BASE, 3);
           //Trigger ADC
-          ADCProcessorTrigger(ADC1_BASE, 2);
+          ADCProcessorTrigger(ADC1_BASE, 3);
           //wait for conversion
-          while(!ADCIntStatus(ADC1_BASE, 2, false)){}
+          while(!ADCIntStatus(ADC1_BASE, 3, false)){}
           //retrieve data
-          ADCSequenceDataGet(ADC1_BASE, 2, ui32ADC1Value);
+          ADCSequenceDataGet(ADC1_BASE, 3, &result1);
 
-          *ui32ADC1Value= (2080*7)/(*ui32ADC1Value);
-          /* Convert digital back to analog */
+          result1= (2080*7)/(result1);
+          return (float)result1;
 
-          char  str[50];
-          sprintf(str,"Right: %u cm\n",ui32ADC1Value);
-          writeStringToUart1(str);
+          //char  str[20]= "                    ";
+         // sprintf(str,"Front: %u cm",result1);
+          //writeStringToUart1(str);
 
 }
 
 
 void hard_initB() {
-    //SysCtlClockSet(SYSCTL_SYSDIV_4 | SYSCTL_USE_PLL | SYSCTL_OSC_MAIN | SYSCTL_XTAL_16MHZ); old code
+    SysCtlClockSet(SYSCTL_SYSDIV_5 | SYSCTL_USE_PLL | SYSCTL_OSC_MAIN | SYSCTL_XTAL_16MHZ);
     //
     // Enable Peripheral Clocks
     //
     //Set the clock
-    SysCtlClockSet(SYSCTL_SYSDIV_1 | SYSCTL_USE_OSC |   SYSCTL_OSC_MAIN | SYSCTL_XTAL_16MHZ);
+   // SysCtlClockSet(SYSCTL_SYSDIV_1 | SYSCTL_USE_OSC |   SYSCTL_OSC_MAIN | SYSCTL_XTAL_16MHZ);
 
+       //This marks the beggining of timer int setup
+    uint32_t ui32Period;
+        // Timer 2 setup code
+        SysCtlPeripheralEnable(SYSCTL_PERIPH_TIMER2);           // enable Timer 2 periph clks
+        TimerConfigure(TIMER2_BASE, TIMER_CFG_PERIODIC);        // cfg Timer 2 mode - periodic
 
+        ui32Period = (SysCtlClockGet() /20);                     // period = CPU clk div 2 (500ms)
+        TimerLoadSet(TIMER2_BASE, TIMER_A, ui32Period);         // set Timer 2 period
+
+        TimerIntEnable(TIMER2_BASE, TIMER_TIMA_TIMEOUT);        // enables Timer 2 to interrupt CPU
+
+        //TimerEnable(TIMER2_BASE, TIMER_A);                      // enable Timer 2
+       //This marks the end of timer int setup
 
     //Configure PWM Clock to match system
     SysCtlPWMClockSet(SYSCTL_PWMDIV_1);
@@ -273,11 +307,10 @@ void writeStringToUart1(char* str)   //write a string to Uart0
 {
     int i;
     for (i = 0; i < strlen(str); i++)
-        UARTCharPut(UART1_BASE,str[i]);
+        UARTCharPutNonBlocking(UART1_BASE,str[i]);
 }
 
 void adc_init(){
-
 
     SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOE);
     //enable ADC0
@@ -299,12 +332,12 @@ void adc_init(){
     //configure Pin PE2 (AIN1) for ADC usage
     GPIOPinTypeADC(GPIO_PORTE_BASE, GPIO_PIN_2);
     //Before Configuring ADC Sequencer 2, it should be OFF
-    ADCSequenceDisable(ADC1_BASE, 2);
+    ADCSequenceDisable(ADC1_BASE, 3);
     //configure ADC sequencer
-    ADCSequenceConfigure(ADC1_BASE, 2, ADC_TRIGGER_PROCESSOR, 0);
+    ADCSequenceConfigure(ADC1_BASE, 3, ADC_TRIGGER_PROCESSOR, 0);
     //configure ADC sequencer steps
-    ADCSequenceStepConfigure(ADC1_BASE, 2, 0, ADC_CTL_CH1|ADC_CTL_IE|ADC_CTL_END);
+    ADCSequenceStepConfigure(ADC1_BASE, 3, 0, ADC_CTL_CH1|ADC_CTL_IE|ADC_CTL_END);
     //enable ADC sequencer
-    ADCSequenceEnable(ADC1_BASE, 2);
+    ADCSequenceEnable(ADC1_BASE, 3);
 
 }
