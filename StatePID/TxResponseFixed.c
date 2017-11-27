4d39d2641a47c17a06aef1e1f0e9796213368722
Nick-Archi <Nick-Archi@users.noreply.github.com>
2017-11-26 19:04:34 -0600

    Fixed some bugs from original c file
    - Fixed the bug where it would produce errors when error value was less than or equal to 15
    - Fixed bug where it would print the ":" only the first time around

---
+++ StatePID/TxResponseFixed.c
@@ -0,0 +1,144 @@
+/*
+ * TxResponse.c
+ *
+ * Local command buffer and other
+ * variables are statically allocated here.
+ */
+
+//----------------------------------------
+// BIOS header files
+//----------------------------------------
+#include <xdc/std.h>    //mandatory - have to include first, for BIOS types
+#include <ti/sysbios/BIOS.h>  //mandatory - if you call APIs like BIOS_start()
+#include <xdc/runtime/Log.h>  //needed for any Log_info() call
+#include <xdc/cfg/global.h>
+
+
+//------------------------------------------
+// TivaWare Header Files
+//------------------------------------------
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <data.h>
+#include <drive.h>
+#include <TxResponse.h>
+#include <main.h>
+#include "inc/hw_types.h"
+#include "inc/hw_memmap.h"
+#include "driverlib/sysctl.h"
+#include "driverlib/gpio.h"
+#include "inc/hw_ints.h"
+#include "driverlib/interrupt.h"
+#include "driverlib/timer.h"
+
+
+//----------------------------------------
+// Macros
+//----------------------------------------
+#define true 1
+#define false 0
+
+
+//------------------------------------------
+// Variables
+//------------------------------------------
+char* TXBufferPtr;
+char* ptrC;
+
+int flag;
+int state;
+
+enum states{start, move, space};
+
+//------------------------------------------
+// Functions
+//------------------------------------------
+
+/*
+ * TxResponse Task
+ * call WriteFrame(char* bufferPointer) to transmit data found at
+ * TX Buffer Pointer. Data is null terminated response string. WriteFrame's
+ * state machine inserts colon, checksum, and CR/LF as it writes character
+ * to UART
+ */
+void TxResponse(){
+
+    while(true){
+
+        Semaphore_pend(TxResponseSema, BIOS_WAIT_FOREVER);
+        WriteFrame(TXBufferPtr);
+
+    }
+
+}
+
+
+/*
+ * Function to copy full buffer
+ * function may wait on the TX_RWLock?
+ */
+void StoreTxBufferPtr_W(char* fullBufferPtr){
+
+    TXBufferPtr = fullBufferPtr;
+    Semaphore_post(TxResponseSema);
+
+
+}
+
+
+/*
+ * Contains state machine to parse through data and look for ' ' (spaces) in the
+ *      data and insert the correct response
+ */
+void WriteFrame(char* bufferPointer){
+
+    ptrC = bufferPointer;   // copy first address
+    state = start;
+
+    while(*ptrC != '\0'){
+        switch(state){
+
+            case start:
+                flag = 0;
+                state = move;
+                break;
+
+            case move:
+                if(*ptrC == ' '){
+                    state = space;
+                    flag++;
+                }
+                else{
+                    // print statement for char
+                    writeCharToUart1(*ptrC);
+                    ptrC++;
+                }
+                break;
+
+            case space:
+                if(flag == 1){
+                    // print statement for colon
+                    writeCharToUart1(':');
+                }
+                else{
+                    // print statement for space
+                    writeCharToUart1(' ');
+                }
+                ptrC++;
+
+                if(*ptrC == '\0'){
+                    // print statement for CR\LF
+                    writeCharToUart1(10);
+                    writeCharToUart1(13);
+                }
+                state = move;
+                break;
+
+            default:
+                state = start;
+        }
+    }
+
+}
---
+++ StatePID/dataFixed.c
@@ -0,0 +1,200 @@
+/*
+ * data.c
+ *
+ * Statically allocate variables for communication between AcquireData and
+ * TxData, including the ping pong buffers.
+ */
+
+
+//----------------------------------------
+// BIOS header files
+//----------------------------------------
+#include <xdc/std.h>    //mandatory - have to include first, for BIOS types
+#include <ti/sysbios/BIOS.h>  //mandatory - if you call APIs like BIOS_start()
+#include <xdc/runtime/Log.h>  //needed for any Log_info() call
+#include <xdc/cfg/global.h>
+
+//------------------------------------------
+// TivaWare Header Files
+//------------------------------------------
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <data.h>
+#include <drive.h>
+#include <TxResponse.h>
+
+
+#include "inc/hw_types.h"
+#include "inc/hw_memmap.h"
+#include "driverlib/sysctl.h"
+#include "driverlib/gpio.h"
+#include "inc/hw_ints.h"
+#include "driverlib/interrupt.h"
+#include "driverlib/timer.h"
+
+
+//----------------------------------------
+// Macros
+//----------------------------------------
+#define true 1
+#define false 0
+#define MAX 255
+
+
+//------------------------------------------
+// Variables
+//------------------------------------------
+/*
+ *  pingPong -> Array for holding right wall values
+ *  bufferFlag -> flag to indicate which buffer to write to
+ *  fullFlag -> flag to indicate when a buffer is full
+ *  buffCount -> used to count till 60
+ *  rightWallErr -> used to receive the right wall error value
+ */
+char pingPong1[64]; // array will hold 2 chars(command), 20 values, 11 spaces, + 1 null value
+char pingPong2[64]; // 2 chars, 20 * 2 chars, 11 chars, 1 char
+
+extern float error;
+extern char comms[200]; // milestone 9
+
+static int bufferFlag = true;
+static int buffCount = 4; // offset value so command takes [0] & [1]
+
+int32_t rightWallErr;
+
+//------------------------------------------
+// Functions
+//------------------------------------------
+
+/*
+ * AcquireData pends on the DataSema
+ *
+ * DataSem -> Called By Timer2 every 100ms once the robot passes the first
+ * "data" line
+ */
+void AcquireData(){
+    while(true){
+
+        Semaphore_pend(DataSema, BIOS_WAIT_FOREVER);
+
+        // get right wall value & convert to hex
+        rightWallErr = (int32_t)error;
+
+        // check if the value is negative or if it's > 255
+        if(rightWallErr < 0){
+            rightWallErr = abs(rightWallErr);
+        }
+
+        if(rightWallErr > MAX){
+            rightWallErr = MAX; // cap the value at 255
+        }
+
+
+        // check what buffer to use
+        /*
+         * Converts the singed int -> hex value which is then stored
+         * in the address location of the pingPong buffer. However, ex)
+         * 246(decimal) => f6 (hex) => 66,36(in ascii) so this will take
+         * up two indices...
+         */
+        if(bufferFlag){ // write to pingPong1
+
+        	if(rightWallErr <= 15){
+        		sprintf(&pingPong1[buffCount], "%c", ' ');
+        		sprintf(&pingPong1[buffCount + 1], "%x", rightWallErr);
+                pingPong1[buffCount + 2] = ' '; // add the space
+        	}
+        	else{
+                sprintf(&pingPong1[buffCount], "%x", rightWallErr);
+                pingPong1[buffCount + 2] = ' '; // add the space
+        	}
+
+            buffCount = buffCount + 3;
+        }
+        else{
+
+        	if(rightWallErr <= 15){
+        		sprintf(&pingPong2[buffCount], "%c", ' ');
+        		sprintf(&pingPong2[buffCount + 1], "%x", rightWallErr);
+                pingPong2[buffCount + 2] = ' '; // add the space
+        	}
+        	else{
+                sprintf(&pingPong2[buffCount], "%x", rightWallErr);
+                pingPong2[buffCount + 2] = ' '; // add the space
+        	}
+
+            buffCount = buffCount + 3;
+        }
+
+        // check if buffer is full
+        if(buffCount <= 61){
+
+            buffCount = 4;  // reset the value
+
+            switch(bufferFlag){
+
+            case 0: // since pingPong2 is full switch to using pingPong1
+                    bufferFlag = true;
+                    pingPong2[0] = 'e';
+                    pingPong2[1] = 'r';
+                    pingPong2[2] = ' ';
+                    pingPong2[3] = ' ';
+                    break;
+
+            case 1:
+                    bufferFlag = false;
+                    pingPong1[0] = 'e';
+                    pingPong1[1] = 'r';
+                    pingPong1[2] = ' ';
+                    pingPong1[3] = ' ';
+                    break;
+
+            default:
+                bufferFlag = false;
+            }
+
+            // post TxDataSema
+            Semaphore_post(TxDataSema);
+
+        }
+    }
+}
+
+
+/*
+ * ISR called by Timer to post DataSem
+ */
+void DataClockFn(){
+
+    Semaphore_post(DataSema);
+
+}
+
+/*
+ *  Call StoreTxBufferPtr, to pass along the full buffer to pointer.
+ */
+void TxData(){
+
+    while(true){
+
+        Semaphore_pend(TxDataSema, BIOS_WAIT_FOREVER);
+
+        // switch the bufferFlag, prepend command, add space too
+        switch(~bufferFlag){
+
+        case 0: // since pingPong2 is full switch to using pingPong1
+                StoreTxBufferPtr_W(pingPong2);
+                break;
+
+        case 1:
+                StoreTxBufferPtr_W(pingPong1);
+                break;
+
+        default:
+            bufferFlag = false;
+        }
+
+    }
+}
